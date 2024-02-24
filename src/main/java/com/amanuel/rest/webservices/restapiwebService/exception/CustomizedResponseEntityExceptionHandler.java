b8ea0dfae38da9488feabfe14d3f8fb19df922c4
amtenu <amannov21@gmail.com>
2024-02-24 14:36:45 -0700

    Customized exception handling to handle our own customized error handling

---
+++ src/main/java/com/amanuel/rest/webservices/restapiwebService/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.amanuel.rest.webservices.restapiwebService.exception;
+
+import java.time.LocalDate;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.amanuel.rest.webservices.restapiwebService.user.UserNotFoundException;
+
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) {
+
+		ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+
+		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) {
+
+		ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+
+		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.BAD_REQUEST);
+	}
+}
---
+++ src/main/java/com/amanuel/rest/webservices/restapiwebService/exception/ErrorDetails.java
@@ -0,0 +1,32 @@
+package com.amanuel.rest.webservices.restapiwebService.exception;
+
+import java.time.LocalDate;
+
+public class ErrorDetails {
+
+	// Structured Error message with time and details
+
+	private LocalDate timeStamp;
+	private String message;
+	private String details;
+
+	public ErrorDetails(LocalDate timeStamp, String message, String details) {
+		super();
+		this.timeStamp = timeStamp;
+		this.message = message;
+		this.details = details;
+	}
+
+	public LocalDate getTimeStamp() {
+		return timeStamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+}
