3b1c8ad46bd374a446dee77c2cd349f782f2b2fc
amtenu <amannov21@gmail.com>
2024-02-24 11:42:27 -0700

    Selecting params from urls using @PathVariable annotation

--- src/main/java/com/amanuel/rest/webservices/restapiwebService/helloworld/HelloworldController.java
+++ src/main/java/com/amanuel/rest/webservices/restapiwebService/helloworld/HelloworldController.java
@@ -1,21 +1,27 @@
 package com.amanuel.rest.webservices.restapiwebService.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloworldController {
-	
-	@GetMapping(path="/hello-world")
-	public String helloworld(){
+
+	@GetMapping(path = "/hello-world")
+	public String helloworld() {
 		return "Hello world";
 	}
-	
-	@GetMapping(path="/hello-world-bean")
+
+	@GetMapping(path = "/hello-world-bean")
 	public helloworldbean helloworldBean() {
-		
+
 		return new helloworldbean("Hello World");
+	}
+
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public helloworldbean helloworldPathVariable(@PathVariable String name) {
+
+		return new helloworldbean(String.format("Hello ,%s", name));
 	}
 
 }
