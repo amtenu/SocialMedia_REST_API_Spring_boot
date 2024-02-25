4d8a8edb91f7de77160889b035b7d519339d4fc0
amtenu <amannov21@gmail.com>
2024-02-25 11:03:43 -0700

    French , Engish,Chinese languages added to the RESTAPI routes

--- src/main/java/com/amanuel/rest/webservices/restapiwebService/helloworld/HelloworldController.java
+++ src/main/java/com/amanuel/rest/webservices/restapiwebService/helloworld/HelloworldController.java
@@ -1,11 +1,22 @@
 package com.amanuel.rest.webservices.restapiwebService.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloworldController {
+
+	private MessageSource messageSource;
+
+	public HelloworldController(MessageSource messageSource) {
+		super();
+		this.messageSource = messageSource;
+	}
 
 	@GetMapping(path = "/hello-world")
 	public String helloworld() {
@@ -22,6 +33,14 @@ public class HelloworldController {
 	public helloworldbean helloworldPathVariable(@PathVariable String name) {
 
 		return new helloworldbean(String.format("Hello ,%s", name));
+	}
+
+	@GetMapping(path = "/hello-world-international")
+	public String helloworldInternatioanalized() {
+
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+//		return "Hello world v2";
 	}
 
 }
