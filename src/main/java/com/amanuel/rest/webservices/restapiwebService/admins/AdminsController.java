fb9a6043492847ccc1f51dfdc0f63d4f87254fa2
amtenu <amannov21@gmail.com>
2024-02-26 10:23:54 -0700

    Refactored the dynamic filtering method

--- src/main/java/com/amanuel/rest/webservices/restapiwebService/admins/AdminsController.java
+++ src/main/java/com/amanuel/rest/webservices/restapiwebService/admins/AdminsController.java
@@ -13,36 +13,34 @@ import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class AdminsController {
-	
+
+	private MappingJacksonValue getFilteredMapping(Object value, String... fields) {
+
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(value);
+
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "password");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("adminsFilter", filter);
+		mappingJacksonValue.setFilters(filters);
+
+		return mappingJacksonValue;
+	}
+
 	@GetMapping("/admins")
 	public MappingJacksonValue getAdmins() {
-		
-		admins admin = new admins(1,"Aman","1234");
-		
-		MappingJacksonValue mappingJacksonValue =new MappingJacksonValue(admin);
-		
-		SimpleBeanPropertyFilter filter=SimpleBeanPropertyFilter.filterOutAllExcept("id","password");
-		FilterProvider filters=new SimpleFilterProvider().addFilter("adminsFilter", filter);
-		
-		mappingJacksonValue.setFilters(filters);
-		
-		return mappingJacksonValue;
-		
+
+		admins admin = new admins(1, "Aman", "1234");
+
+		return getFilteredMapping(admin, "id", "passowrd");
+
 	}
-	
+
 	@GetMapping("/admins_list")
 	public MappingJacksonValue getAdminsAsList() {
-		
-		List<admins> adminsList = Arrays.asList(new admins(1,"Aman","1234"),new admins(1,"Solo","1234"));
-		
-		MappingJacksonValue mappingJacksonValue =new MappingJacksonValue(adminsList);
-		
-		SimpleBeanPropertyFilter filter=SimpleBeanPropertyFilter.filterOutAllExcept("id","name");
-		FilterProvider filters=new SimpleFilterProvider().addFilter("adminsFilter", filter);
-		
-		mappingJacksonValue.setFilters(filters);
-		return mappingJacksonValue;
-		
+
+		List<admins> adminsList = Arrays.asList(new admins(1, "Aman", "1234"), new admins(1, "Solo", "1234"));
+
+		return getFilteredMapping(adminsList,"name","password");
+
 	}
 
 }
